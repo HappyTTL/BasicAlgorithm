694082180cfee8ce5cb2dda168d8f5cf47284342
HappyTTL <lemonttlu@gmail.com>
2016-10-03 14:32:47 -0400

    Add more binary tree operation

--- src/Tree.java
+++ src/Tree.java
@@ -1,10 +1,49 @@
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 /**
  * Created by Tingting on 10/2/16.
  */
+
+/**
+ * Design an iterator over a binary search tree with the following rules:
+
+ Elements are visited in ascending order (i.e. an in-order traversal)
+ next() and hasNext() queries run in O(1) time in average.
+ */
+class BSTIterator{
+    public Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<TreeNode>();
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+    //@return: True if there has next node, or false
+    public boolean hasNext() {
+        // write your code here
+        return !stack.isEmpty();
+    }
+
+    //@return: return next node
+    public TreeNode next() {
+        // write your code here
+        TreeNode node = stack.pop();
+        TreeNode result = node;
+        if (node.right != null) {
+            node = node.right;
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+        }
+        return result;
+    }
+}
 public class Tree {
     /**
      * minimum and maximum depth of binary tree
@@ -42,7 +81,7 @@ public class Tree {
     }
 
     /**
-     * Tree traversal, preorder, inorder and postorder, both recursive and non-recursive
+     * Tree traversal, preorder, inorder and postorder and levelorder, both recursive and non-recursive
      * @param args
      */
 
@@ -170,6 +209,257 @@ public class Tree {
         return result;
     }
 
+    public ArrayList<Integer> levelorderTraversalNoRecur(TreeNode root) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            result.add(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Zigzag level Order Traversal
+     */
+    public ArrayList<ArrayList<Integer>> zigzagTraversal(TreeNode root) {
+        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int size = queue.size();
+        int count = 0;
+        boolean flag = true;
+        ArrayList<Integer> list = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (flag) {
+                list.add(node.val);
+            } else {
+                list.add(0, node.val);
+            }
+            count++;
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+            if (count == size) {
+                size = queue.size();
+                result.add(list);
+                list = new ArrayList<Integer>();
+                count = 0;
+                flag = !flag;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Check if two trees are identical, or tweaked Identical
+     */
+
+    public boolean isIdentical(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (node1.val == node2.val) {
+            return isIdentical(node1.left, node2.left) && isIdentical(node1.right, node2.right);
+        }
+        return false;
+    }
+
+    /**
+     * Check two given binary trees are identical or not. Assuming any number of tweaks are allowed.
+     * A tweak is defined as a swap of the children of one node in the tree.
+     * @param node1
+     * @param node2
+     * @return
+     */
+    public boolean isTweakedIdentical(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (node1.val == node2.val) {
+            return ((isTweakedIdentical(node1.right, node2.right) && isTweakedIdentical(node1.left, node2.left)) || (isTweakedIdentical(node1.left, node2.right) && isTweakedIdentical(node1.right, node2.left)));
+        }
+        return false;
+    }
+
+    /**
+     * Symmetric Binary Tree: check if a tree is symmetric around its center
+     */
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return checkSymmetric(root.left, root.right);
+    }
+    private boolean checkSymmetric(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        return node1.val == node2.val && (checkSymmetric(node1.left, node2.right) && checkSymmetric(node1.right, node2.left));
+    }
+
+    /**
+     * Is Complete: Check a binary tree is completed or not. A complete binary tree is a binary tree that every level
+     * is completed filled except the deepest level. In the deepest level, all nodes must be as left as possible.
+     */
+    // Recursion Method: define a result type with depth, isFull, isComplete, if leftdepth == rightdepth, left needs
+    // to be full, right needs to be complete, if leftdepth = rightdepth + 1, right needs to be full, left needs to be
+    // complete
+    // Here we give non recursion method: using a queue
+    public boolean isComplete(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        int index = 0;
+        queue.add(root);
+        while (index < queue.size()) {
+            TreeNode node = queue.get(index);
+            index++;
+            if (node == null) {
+                continue;
+            } else {
+                queue.offer(node.left);
+                queue.offer(node.right);
+            }
+        }
+        while (queue.get(queue.size() - 1) == null) {
+            queue.remove(queue.size() - 1);
+        }
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Is balanced: Given a binary tree, determine if it is height-balanced.
+
+     For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees
+     of every node never differ by more than 1.
+     */
+    public boolean isBalenced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        if (getHeight(root) == -1) {
+            return false;
+        }
+        return true;
+    }
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        if (left == -1 || right == -1) {
+            return -1;
+        }
+        if (Math.abs(left - right) > 1) {
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+
+    /**
+     * Valid Binary Search Tree:
+     */
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return isValidBSThelper(root, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY);
+    }
+    private boolean isValidBSThelper(TreeNode root, Double min, Double max) {
+        if (root == null) {
+            return true;
+        }
+        if (root.val >= max || root.val <= min) {
+            return false;
+        }
+        boolean isLeftValid = isValidBSThelper(root.left, min, (double) root.val);
+        boolean isRightValid = isValidBSThelper(root.right, (double) root.val, max);
+        return isLeftValid && isRightValid;
+    }
+    /**
+     * Inorder Successor in Binary Search Tree
+     */
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode A) {
+        TreeNode successor = null;
+        while (root != null && root != A) {
+            if (root.val > A.val) {
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        if (root == null) {
+            return null;
+        }
+        if (root.right == null) {
+            return successor;
+        }
+        root = root.right;
+        if (root.left != null) {
+            return root.left;
+        }
+        return root;
+    }
+    /**
+     * Lowest Common Ancestor I, II, III, LCA with parent reference, use a hashset to record A and A's all parents
+     * then for B do the same thing to check if B's parent is already in the hashset, if yes return, no, return null
+     *
+     */
+    public TreeNode LCA(TreeNode root, TreeNode A, TreeNode B) {
+        if (root == null || root == A || root == B) {
+            return root;
+        }
+        TreeNode left = LCA(root.left, A, B);
+        TreeNode right = LCA(root.right, A, B);
+        if (left != null && right != null) {
+            return root;
+        }
+        if (left != null) {
+            return left;
+        }
+        if (right != null) {
+            return right;
+        }
+        return null;
+    }
+
+
     /**
      * Invert Binary Tree
      * @param args
@@ -184,6 +474,90 @@ public class Tree {
         invertBinaryTree(root.left);
         invertBinaryTree(root.right);
     }
+
+
+    /**
+     * Path Sum 类问题
+     * @param args
+     */
+
+
+    /**
+     * Binary Tree Maximum Path Sum: Given a binary tree, find the maximum path sum.
+
+     The path may start and end at any node in the tree.
+     * @param args
+     */
+
+    public int maxPathSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int[] sum = new int[1];
+        sum[0] = Integer.MIN_VALUE;
+        //singlePath is the maximal sum from root to one side
+        int singlePath = maxPathSumHelper(root, sum);
+        return sum[0];
+    }
+    private int maxPathSumHelper(TreeNode root, int[] sum) {
+        if (root == null) {
+            return 0;
+        }
+        int leftSinglePath = Math.max(maxPathSumHelper(root.left, sum), 0);
+        int rightSinglePath = Math.max(maxPathSumHelper(root.right, sum), 0);
+        int singlePath = Math.max(leftSinglePath, rightSinglePath) + root.val;
+        int twoPath = leftSinglePath + root.val + rightSinglePath;
+        sum[0] = Math.max(sum[0], Math.max(singlePath, twoPath));
+        return singlePath;
+    }
+
+    /**
+     * maxPathSum II: Given a binary tree, find the maximum path sum from root.
+
+     The path may end at any node in the tree and contain at least one node in it.
+     */
+    public int maxPathSumII(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(maxPathSumII(root.left), 0);
+        int right = Math.max(maxPathSumII(root.right), 0);
+        return Math.max(left, right) + root.val;
+    }
+
+    /**
+     * Path Sum: Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all
+     * the values along the path equals the given sum.
+     * @param args
+     */
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        if (sum == root.val && root.left == null && root.right == null) {
+            return true;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+
+    /**
+     * Tree Reconstruction Problem: From sorted array, from pre and in order traversal, from post and in order traversal
+     * Tree Serialization and deserialization
+     * @param args
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        return sortedArrayToBST(nums, 0, nums.length - 1) {
+            
+        }
+    }
+
+
+
+
+
     public static void main(String args[]) {
         TreeNode root = new TreeNode(8);
         TreeNode left1 = new TreeNode(3);
@@ -237,32 +611,78 @@ public class Tree {
          *                         / \  /
          *                        4  7 13
          */
+        TreeNode rootSys = new TreeNode(8);
+        TreeNode leftSys = new TreeNode(3);
+        rootSys.left = leftSys;
+        TreeNode rightSys = new TreeNode(3);
+        rootSys.right = rightSys;
+
         Tree t = new Tree();
         int minimumDepth = t.minDepth(root);
         int maximumDepth = t.maxDepth(root);
         ArrayList<Integer> preorder = t.preorderTraversalNoRecur(root);
         ArrayList<Integer> inorder = t.inorderTraversalNoRecur(root);
         ArrayList<Integer> postorder = t.postorderTraversalNoRecur(root);
-        System.out.println("minimum and maximum Depth:");
-        System.out.println(minimumDepth);
-        System.out.println(maximumDepth);
-        System.out.println("preorder traversal:");
-        for (int i = 0; i < preorder.size(); i++) {
-            System.out.println(preorder.get(i));
-        }
-        System.out.println("inorder traversal:");
-        for (int i = 0; i < inorder.size(); i++) {
-            System.out.println(inorder.get(i));
-        }
-        System.out.println("postorder traversal:");
-        for (int i = 0; i < postorder.size(); i++) {
-            System.out.println(postorder.get(i));
+        ArrayList<Integer> levelorder = t.levelorderTraversalNoRecur(root);
+        boolean isIdentical = t.isIdentical(root, rootDup);
+        if (isIdentical) {
+            System.out.println("two trees are identical");
         }
         t.invertBinaryTree(rootDup);
-        ArrayList<Integer> preorderInvert = t.preorderTraversalNoRecur(rootDup);
-        System.out.println("Inverted Tree preorder traversal:");
-        for (int i = 0; i < preorderInvert.size(); i++) {
-            System.out.println(preorderInvert.get(i));
+        boolean isTweakedIdentical = t.isTweakedIdentical(root, rootDup);
+        if (isTweakedIdentical) {
+            System.out.println("two trees are tweaked identical");
         }
+        boolean isSys = t.isSymmetric(rootSys);
+        if (isSys) {
+            System.out.println("the tree is symmetric");
+        } else {
+            System.out.println("the tree is not symmetric");
+        }
+        boolean isCom1 = t.isComplete(root);
+        boolean isCom2 = t.isComplete(rootSys);
+        if (isCom1) {
+            System.out.println("the first tree is complete");
+        } else {
+            System.out.println("the first tree is not complete");
+        }
+        if (isCom2) {
+            System.out.println("the second tree is complete");
+        } else {
+            System.out.println("the second tree is not complete");
+        }
+        boolean isBal = t.isBalenced(root);
+        if (isBal) {
+            System.out.println("the tree is balanced");
+        } else {
+            System.out.println("the tree is not balanced");
+        }
+        int maxPathSum = t.maxPathSum(root);
+        System.out.println(maxPathSum);
+//        System.out.println("minimum and maximum Depth:");
+//        System.out.println(minimumDepth);
+//        System.out.println(maximumDepth);
+//        System.out.println("preorder traversal:");
+//        for (int i = 0; i < preorder.size(); i++) {
+//            System.out.println(preorder.get(i));
+//        }
+//        System.out.println("inorder traversal:");
+//        for (int i = 0; i < inorder.size(); i++) {
+//            System.out.println(inorder.get(i));
+//        }
+//        System.out.println("postorder traversal:");
+//        for (int i = 0; i < postorder.size(); i++) {
+//            System.out.println(postorder.get(i));
+//        }
+//        System.out.println("levelorder traversal:");
+//        for (int i = 0; i < levelorder.size(); i++) {
+//            System.out.println(levelorder.get(i));
+//        }
+//        t.invertBinaryTree(rootDup);
+//        ArrayList<Integer> preorderInvert = t.preorderTraversalNoRecur(rootDup);
+//        System.out.println("Inverted Tree preorder traversal:");
+//        for (int i = 0; i < preorderInvert.size(); i++) {
+//            System.out.println(preorderInvert.get(i));
+//        }
     }
 }
